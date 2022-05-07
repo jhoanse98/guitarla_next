65bf44f37f66b28d72b02ac3e83111ceea988699
Sebastian Marin <sebastian.marin.val@gmail.com>
2022-05-07 14:01:56 -0500

    Creación de estilos para el header y creación de Footer y vista de nosotros

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from '../styles/Header.module.css'
+
+const Header = () => {
+    return (
+
+        <header className={styles.header}>
+            <div className="Contenedor">
+                <div className={styles.barra}>
+                    <Link href="/">
+                        <Image width={300} height={100} src="/img/logo.svg" alt="imagen logo" />
+                    </Link>
+
+                    <nav className={styles.navegacion}>
+                        <Link href="/">Inicio</Link>
+                        <Link href="/nosotros">Nosotros</Link>
+                        <Link href="/blog">Blog</Link>
+                        <Link href="/tienda">Tienda</Link>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+}
+ 
+export default Header;
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,10 +1,25 @@
+import Layout from '../components/Layout'
+import Image from 'next/image'
+import styles from '../styles/Nosotros.module.css'
+
 const Nosotros = () => {
     return ( 
-    <div>
-        <h1>Desde Nosotros</h1>
+    <Layout
+        pagina="Nosotros"
+    >
+        <main className='contenedor'>
+            <h2 className='heading'>Nosotros</h2>
+            <div className={styles.contenido}>
 
-        <a href="/" >Ir a Inicio</a>
-    </div>
+                <Image layout="responsive" width={600} height={450} src="/img/nosotros.jpg" alt="nosotros logo"/>
+                <div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque alias temporibus quia possimus quibusdam soluta reprehenderit sapiente dolorum delectus! Voluptate pariatur, sequi asperiores aliquam delectus consequuntur laboriosam aperiam veritatis hic! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit fuga, maiores, repellat quod, sed voluptatum placeat at laboriosam cum nam nihil error aspernatur aperiam accusamus. In praesentium laudantium odit aspernatur.</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio beatae est sit magni non mollitia totam laudantium eveniet officiis fugiat impedit a hic et atque cupiditate, magnam quo, veniam in! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam tenetur deserunt repudiandae ad corrupti perspiciatis totam architecto ducimus blanditiis velit, exercitationem illum ipsum repellendus eos cupiditate iusto minima! Temporibus, voluptates!</p>
+                </div>
+            </div>
+        </main>
+    </Layout>
 );
 }
  
